de59cdc7f7778a2079299044205fd03e2b2e2716
bishalbera <bishalbera2@gmail.com>
2024-06-08 10:23:52 +0530

    made the grpc server

--- grpc_server.go
+++ grpc_server.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"context"
+
+	"github.com/bishalbera/catfactmicroservice/proto"
+)
+
+type GRPCCatFactServer struct {
+	svc Service
+	proto.UnimplementedCatFactServer
+}
+
+func NewGRPCCatFactServer(svc Service) *GRPCCatFactServer {
+	return &GRPCCatFactServer{
+		svc: svc,
+	}
+}
+
+func (s *GRPCCatFactServer) GetCatFact(ctx context.Context, req *proto.CatFactRequest) (*proto.CatFactResponse, error) {
+
+	fact,err:= s.svc.GetCatFact(ctx)	
+	if err != nil {
+		return nil,err
+	}
+
+	resp := &proto.CatFactResponse{
+		Fact: fact.Fact,
+	}
+
+	return resp, err
+}
